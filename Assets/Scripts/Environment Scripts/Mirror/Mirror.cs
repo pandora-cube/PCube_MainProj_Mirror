eb94a318bf733ca0cfc9bf640b0c6c2684ca2e03
crappycake <kjh357012@gmail.com>
2024-05-31 09:29:28 +0900

    Initial commit
    Movement / Dash set up

---
+++ Assets/Scripts/Environment Scripts/Mirror/Mirror.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    public InteractableObject interactableObject;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
