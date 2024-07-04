1328819dacd53d17305575473b88e927ba226b66
CW-YI <cwyi6104@gmail.com>
2024-07-04 16:49:55 +0900

    Add inventory item tooltip
    마우스 오버 시 아이템 이름/info 나타남

--- Assets/Scripts/Player Scripts/Inventory/Slot.cs
+++ Assets/Scripts/Player Scripts/Inventory/Slot.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
-public class Slot : MonoBehaviour, IPointerClickHandler
+public class Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] Image image;
     [SerializeField] Inventory inventory;
 
-    [SerializeField] TMP
+    [SerializeField] private TextMeshProUGUI NameText;
+    [SerializeField] private TextMeshProUGUI InfoText;
+    [SerializeField] private GameObject InfoScreen;
+
+    [SerializeField] RectTransform infoRect;
+    RectTransform slotRect;
+
     private Item _item;
     public Item item {
         get { return _item; }
@@ -24,8 +31,30 @@ public class Slot : MonoBehaviour, IPointerClickHandler
             else image.color = new Color(1, 1, 1, 0);
         }
     }
+
+    void Start()
+    {
+        slotRect = GetComponent<RectTransform>();
+    }
     public void OnPointerClick(PointerEventData eventData)
     {
         if (_item != null) inventory.UseItem(item);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_item != null)
+        {
+            NameText.text = _item.itemName;
+            InfoText.text = _item.itemInfo;
+
+            infoRect.position = slotRect.position;
+            InfoScreen.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        InfoScreen.SetActive(false);
     }
 }
