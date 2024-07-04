952df9d67fb4223a71ea3456bfc933d8e0c134c0
crappycake <kjh357012@gmail.com>
2024-07-04 16:58:29 +0900

    added transformation
    no delay made, jump bug still exists

--- Assets/Scripts/Environment Scripts/Mirror/Mirror.cs
+++ Assets/Scripts/Environment Scripts/Mirror/Mirror.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mirror : MonoBehaviour
+public class Mirror : MonoBehaviour, IInteractable
 {
-    public void Interact()
+    [SerializeField] private string interactText;
+    public void Interact(Transform interactorTransform)
     {
         PlayerController playerController = FindObjectOfType<PlayerController>();
         playerController.Transform();
         Debug.Log("Interacted");
+    }
+
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;                         
     }
 }
