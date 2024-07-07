75f4f4849dcb91bb55b39d8b6074cc0a03fb99f1
crappycake <kjh357012@gmail.com>
2024-07-07 20:58:57 +0900

    added basic item pick up

---
+++ Assets/Scripts/Environment Scripts/Pick up items/Pickup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string interactText;
+    [SerializeField] private Item item;
+    // Start is called before the first frame update
+    public void Interact(Transform interactorTransform)
+    {
+        Inventory inventory = FindObjectOfType<Inventory>();
+        inventory.AddItem(item);
+        Destroy(gameObject);
+        Debug.Log("Interacted");
+    }
+
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
