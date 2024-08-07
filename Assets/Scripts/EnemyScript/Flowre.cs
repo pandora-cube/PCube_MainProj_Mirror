4fe06bf68c7c8ca628d7c94f3df0ebd16cfb4e4c
crappycake <kjh357012@gmail.com>
2024-08-07 17:24:41 +0900

    ADD basic logic for Flowre

---
+++ Assets/Scripts/EnemyScript/Flowre.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flowre : MonoBehaviour, IDamageable
+{
+    private PlayerController playerController;
+    [SerializeField] private CapsuleCollider2D openCapsuleCollider2D;
+
+    private bool isOpen = false;
+    private float playerDetectionRadius = 1f;
+    private float attackDelay = 1f;
+
+    public float maxHealth { get; set;}
+    public float currentHealth { get; set; }
+
+    void Awake()
+    {
+        playerController = FindAnyObjectByType<PlayerController>();
+    }
+
+    void Update()
+    {
+        if (!playerController.isGhost) CloseWhenPlayerIsNormal();
+
+            
+        else if (playerController.isGhost)
+        {
+            DetectPlayer();
+
+            if (isOpen) StartCoroutine(AttackPlayer());
+        }
+    }
+    
+    void CloseWhenPlayerIsNormal()
+    {
+        openCapsuleCollider2D.enabled = false;
+        isOpen = false;
+    }
+
+    void DetectPlayer()
+    {
+        //TO-DO: ADD ANIM TRIGGER FOR OPENING
+        openCapsuleCollider2D.enabled = true; //also allows Flowre to be attackable.
+        RaycastHit2D circleCast = Physics2D.CircleCast(transform.position, playerDetectionRadius, Vector2.up);
+
+        if (circleCast.collider.gameObject.CompareTag("Player")) isOpen = true;
+    }
+
+    IEnumerator AttackPlayer()
+    {
+        //TO-DO: ADD ANIM TRIGGER FOR ATTACK
+        yield return new WaitForSeconds(attackDelay);
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        //TO-DO: ADD CLOSING ANIM
+        isOpen = false;
+    }
+
+    public void Die()
+    {
+        //TO-DO: ADD DEATH ANIM
+        Destroy(gameObject);
+    }
+}
