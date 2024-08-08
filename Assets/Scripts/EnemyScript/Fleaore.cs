219e55174f8ad960214d6638db75ccb9d39c6fa7
crappycake <kjh357012@gmail.com>
2024-08-08 17:34:33 +0900

    ADD fleaore & radix, working on basic implementation

--- Assets/Scripts/EnemyScript/Fleaore.cs
+++ Assets/Scripts/EnemyScript/Fleaore.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Fleaore : MonoBehaviour
 {
+    [SerializeField] private Radix connectedRadix;
+    
     // Start is called before the first frame update
     void Start()
     {
