c3539425a44d1504655505fa2c1a2a4a1bcfa83a
crappycake <kjh357012@gmail.com>
2024-08-08 17:35:35 +0900

    Update Fleaore.cs

--- Assets/Scripts/EnemyScript/Fleaore.cs
+++ Assets/Scripts/EnemyScript/Fleaore.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class Fleaore : MonoBehaviour
 {
     [SerializeField] private Radix connectedRadix;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
