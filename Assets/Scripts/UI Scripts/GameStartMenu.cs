b568e8d550cb2fd0a7dbf03bec4c799b446ce3d3
crappycake <kjh357012@gmail.com>
2024-09-26 18:15:38 +0900

    BUG FIX
    - 이어하기 첫 실행 때는 안 뜨게 막아달라고

--- Assets/Scripts/UI Scripts/GameStartMenu.cs
+++ Assets/Scripts/UI Scripts/GameStartMenu.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStartMenu : MonoBehaviour
 {
     const int FORST_STAGE_MAP1 = 2;
+
+    [SerializeField] private GameObject popupPanel;
+    public void OnStartCliked()
+    {
+        if (ProgressData.Instance.playerData.currentChapter == 1 && ProgressData.Instance.playerData.currentStage == 1)
+        {
+            StartFromBeginning();
+        }
+        else
+        {
+            popupPanel.SetActive(true);
+        }
+
+    }
     public void StartFromBeginning()
     {
         SceneManager.LoadScene(FORST_STAGE_MAP1);
