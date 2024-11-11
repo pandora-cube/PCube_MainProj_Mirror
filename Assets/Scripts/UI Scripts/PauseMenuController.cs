ec23dc1aded7a26b7010624525966fdf4c073c93
crappycake <kjh357012@gmail.com>
2024-11-11 17:56:05 +0900

    FIX pause menu UI to use ESC button as well

--- Assets/Scripts/UI Scripts/PauseMenuController.cs
+++ Assets/Scripts/UI Scripts/PauseMenuController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -8,12 +10,15 @@ public class PauseMenuController : MonoBehaviour
 {
     public GameObject topPauseMenu;
     public GameObject settingsMenu;
+    public GameObject[] subMenus;
 
     private bool isPaused = false;
+    int activatedSubMenuIdx;
 
     void Awake()
     {
         isPaused = false;
+        activatedSubMenuIdx = -1;
     }
 
     public void OnPauseButtonPressed(InputAction.CallbackContext ctx)
@@ -25,10 +30,29 @@ public class PauseMenuController : MonoBehaviour
         {
             TogglePause();
         }
-        //unpause only if player is looking at the top pause menu
         else
         {
-            if (settingsMenu.activeSelf) TogglePause();
+            bool isAtSettingsSubMenu = IsAllSubMenusClosed();
+
+            //player is not looking at any of the settings submenu
+            if (!isAtSettingsSubMenu)
+            {
+                //unpause only if player is looking at the top pause menu
+                if (!settingsMenu.activeSelf && isPaused) TogglePause();
+
+                //player is only looking at the top settings menu. Press esc to toggle pause menu
+                else if (settingsMenu.activeSelf && isPaused)
+                {
+                    settingsMenu.SetActive(false);
+                    topPauseMenu.SetActive(true);
+                }
+            }
+            else
+            {
+                //player is in the settings submenu
+                Debug.Log("Yes~");
+                CloseActivatedSubMenu();
+            }
         }
     }
 
@@ -48,7 +72,27 @@ public class PauseMenuController : MonoBehaviour
         }
     }
 
-    
+    private bool IsAllSubMenusClosed()
+    {
+        //iterate through the array of submenus
+        //if submenu is active, keep the index of activated sub menu and return true
+        for (int i = 0; i < subMenus.Length; ++i)
+        {
+            if (subMenus[i].activeSelf)
+            {
+                activatedSubMenuIdx = i;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void CloseActivatedSubMenu()
+    {
+        subMenus[activatedSubMenuIdx].SetActive(false); //turn off the submenu
+        settingsMenu.SetActive(true); //turn on the top pause menu
+    }
 
     public void RestartCurrentMap()
     {
--- Assets/Scripts/UI Scripts/SettingsUI.cs
+++ Assets/Scripts/UI Scripts/SettingsUI.cs
@@ -4,36 +4,9 @@ using UnityEngine;
 
 public class SettingsUI : MonoBehaviour
 {
-    public GameObject topPauseMenu;
-    public GameObject[] subMenus;
-    public bool isSubMenuActivated = false;
-    private int activatedSubMenuIdx = 0;
-
-    public bool isSettingsMenuActivated;
 
     void Awake()
     {
-        isSubMenuActivated = false;
-        activatedSubMenuIdx = 0;
+        
     }
-
-    private void FindActivatedSubMenu()
-    {
-        //iterate through the array of submenus
-        //if submenu is active, set the bool as true and keep the index of activated sub menu.
-        for (int i = 0; i < subMenus.Length; ++i)
-        {
-            if (!subMenus[i].activeSelf) continue;
-
-            isSubMenuActivated = true;
-            activatedSubMenuIdx = i;
-        }
-    }
-
-    private void CloseActivatedSubMenu()
-    {
-        subMenus[activatedSubMenuIdx].SetActive(false); //turn off the submenu
-        topPauseMenu.SetActive(true); //turn on the top pause menu
-    }
-
 }
